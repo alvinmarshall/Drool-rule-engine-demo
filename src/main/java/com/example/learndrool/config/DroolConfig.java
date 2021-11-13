d5b58dc9d48de0abfc6df8b18f6cc34b8975186a
Bik-Krlvn <kelvinbirikorang@gmail.com>
2021-11-13 21:01:31 +0000

    clean up config

--- src/main/java/com/example/learndrool/config/DroolConfig.java
+++ src/main/java/com/example/learndrool/config/DroolConfig.java
@@ -1,14 +1,12 @@
 package com.example.learndrool.config;
 
-import org.drools.decisiontable.DecisionTableProviderImpl;
 import org.kie.api.KieServices;
-import org.kie.api.builder.*;
-import org.kie.api.io.Resource;
+import org.kie.api.builder.KieBuilder;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.builder.KieModule;
+import org.kie.api.builder.KieRepository;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-import org.kie.internal.builder.DecisionTableConfiguration;
-import org.kie.internal.builder.DecisionTableInputType;
-import org.kie.internal.builder.KnowledgeBuilderFactory;
 import org.kie.internal.io.ResourceFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -49,36 +47,10 @@ public class DroolConfig {
     @Bean
     public KieSession kieSession() {
         getKieRepository();
-        KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
-//        kieFileSystem.write(ResourceFactory.newClassPathResource(RULE_PATH + "order.drl"));
-        kieFileSystem.write(ResourceFactory.newClassPathResource(RULE_PATH + "customer_discount.xlsx"));
-        kieFileSystem.write(ResourceFactory.newClassPathResource(RULE_PATH + "employee_tax.xlsx"));
-        KieBuilder kieBuilder = kieServices.newKieBuilder(kieFileSystem);
+        KieBuilder kieBuilder = kieServices.newKieBuilder(getKieFileSystem());
         kieBuilder.buildAll();
         KieModule kieModule = kieBuilder.getKieModule();
         KieContainer kieContainer = kieServices.newKieContainer(kieModule.getReleaseId());
         return kieContainer.newKieSession();
-    }
-
-    public KieSession kieSession(Resource dt) {
-        KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
-        kieFileSystem.write(dt);
-        KieBuilder kieBuilder = kieServices.newKieBuilder(kieFileSystem);
-        kieBuilder.buildAll();
-        KieRepository repository = kieServices.getRepository();
-        ReleaseId defaultReleaseId = repository.getDefaultReleaseId();
-        KieContainer kieContainer = kieServices.newKieContainer(defaultReleaseId);
-        return kieContainer.newKieSession();
-    }
-
-    public String getDrlFromExcel(String excelFile) {
-        DecisionTableConfiguration configuration = KnowledgeBuilderFactory.newDecisionTableConfiguration();
-        configuration.setInputType(DecisionTableInputType.XLS);
-
-        Resource dt = ResourceFactory.newClassPathResource(excelFile, getClass());
-
-        DecisionTableProviderImpl decisionTableProvider = new DecisionTableProviderImpl();
-
-        return decisionTableProvider.loadFromResource(dt, null);
     }
 }
